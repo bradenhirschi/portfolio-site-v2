f027b6f54cb5930c267f6a96f9d3d1f2f3e9c647
Braden Hirschi <bradenhirschi29@gmail.com>
2025-11-04 21:22:46 -0600

    Initial commit of basic site with gba rom

---
+++ app/(home)/hero-section.tsx
@@ -0,0 +1,23 @@
+  "use client"
+
+const HeroSection = () => {
+
+
+  return (
+    <section className="py-36">
+      <div className="mx-auto max-w-4xl flex flex-col">
+        <h1 className="text-5xl">Braden Hirschi</h1>
+        <h3 className="text-xl mb-4">Software Engineer</h3>
+        <p>
+          I'm a software engineer from Missouri who enjoys building software of
+          all kinds. At my day job I'm a full-stack engineer at Expedia Group
+          building marketing tools to help our partners power travel worldwide.
+          This is a space for me to share things I'm learning and creating.
+        </p>
+        <a href={`https://gba.ninja/?autorun=https://bradenhirschi.com/flap-dx.gba`}>plays</a>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./hero-section";
+import ProjectsSection from "./projects-section";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <HeroSection />
+      {/* <ProjectsSection /> */}
+    </main>
+  );
+}
