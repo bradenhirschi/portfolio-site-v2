d197b0f81a7bc51d96bdc0069b8023fabe18e12b
Braden Hirschi <bradenhirschi29@gmail.com>
2026-02-19 12:51:25 -0600

    Update styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,6 @@ export const metadata: Metadata = {
   },
 }
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
-
 export default function RootLayout({
   children,
 }: {
@@ -44,11 +42,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-      )}
+      className='text-zinc-950 bg-white dark:text-white dark:bg-zinc-950'
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-3xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
